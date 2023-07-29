59092e7afff6e3031cfa16d28c733157b5bbdb25
md nabab sharif <mdnababsharifdiu@gmail.com>
2023-07-29 12:40:37 +0600

    ten section done

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+$('.slide').slick({
+  arrows: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  prevArrow: '<i class="fa-solid fa-angle-left left"></i>',
+  nextArrow: '<i class="fa-solid fa-angle-right right"></i>'
+})
